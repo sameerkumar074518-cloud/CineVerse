e71111ac86a65b90e872ed19e2a1a8fe3619c005
sameerkumar074518-cloud <sameerkumar074518@gmail.com>
2026-05-11 22:55:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -119,9 +119,6 @@ app.post("/register", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-app.get("/forgot-password", (req, res) => {
-  res.send("Forgot password route exists. Use POST request.");
-});
 
 app.post("/forgot-password", async (req, res) => {
   try {
@@ -352,8 +349,6 @@ app.delete("/mylist/:id", auth, async (req, res) => {
 /* START */
 /* ================= */
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} 🚀`);
+app.listen(5000, () => {
+  console.log("Server running on port 5000 🚀");
 });
