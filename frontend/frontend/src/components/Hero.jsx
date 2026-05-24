d0c5f09aa9a5d3f7843a3f489244e946e5c09fc0
sameerkumar074518-cloud <sameerkumar074518@gmail.com>
2026-05-24 21:55:30 +0530

    Update Hero.jsx

--- frontend/frontend/src/components/Hero.jsx
+++ frontend/frontend/src/components/Hero.jsx
@@ -81,7 +81,7 @@ const previewTimerRef = useRef(null);
     "Dhurandhar: The Revenge": {
       full: "Dhurandhar: The Revenge",
       desc: "Action • Spy • Thriller",
-      preview: 4269
+      preview: 4267.5
     },
     "Paatal Lok": {
       full: "Paatal Lok",
